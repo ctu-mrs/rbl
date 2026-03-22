6e65895f442b27283086057488ca33b299a81767
manuelboldrer <manuel.boldrer@gmail.com>
2026-03-22 16:48:22 +0100

    some cleaning

--- tmux/mrs_9/sim1/compute.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-awk -F',' 'NR>1 {sum+=$2} END {print sum/(NR-1)}' metric
-awk -F',' 'NR>1 {sum+=$3} END {print sum/(NR-1)}' metric
-awk -F',' 'NR>1 {sum+=$4} END {print sum/(NR-1)}' metric
