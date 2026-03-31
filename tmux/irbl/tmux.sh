50193283cd05e01b4a67c8b689d84b0aabc93bd6
Manuel Boldrer <Manuel Boldrer@Manuel Boldrer.com>
2026-03-31 11:17:08 +0200

    update

--- tmux/irbl/tmux.sh
+++ tmux/irbl/tmux.sh
@@ -70,8 +70,8 @@ input=(
 '
   'tf1' './config/world_origin_tf.sh
 '
-  'zenoh' 'export ZENOH_ROUTER_CONFIG_URI=`ros2 pkg prefix mrs_uav_deployment`/share/mrs_uav_deployment/config/zenoh/uav_router.json5; ros2 run rmw_zenoh_cpp rmw_zenohd
-'
+  'zenoh' 'export ZENOH_ROUTER_CONFIG_URI=/home/mrs/zenoh/uav_router.json5; ros2 run rmw_zenoh_cpp rmw_zenohd
+# '
   # 'Rviz' 'rviz2 -d rviz.rviz
 # '
 )
