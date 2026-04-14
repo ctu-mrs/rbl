246a4acea72b9319db836b3e1cb3eafc05d46d28
manuelboldrer <manuel.boldrer@gmail.com>
2026-04-14 10:27:26 +0200

    override in launch. totest

--- ros_packages/nodelet/launch/rbl_controller.launch.py
+++ ros_packages/nodelet/launch/rbl_controller.launch.py
@@ -1,5 +1,4 @@
 import os
-import launch
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
@@ -22,51 +21,51 @@ def generate_launch_description():
     # -------------------------------------------------
     # UAV name
     # -------------------------------------------------
-
     ld.add_action(DeclareLaunchArgument(
         "uav_name",
         default_value=EnvironmentVariable("UAV_NAME", default_value="uav1")
     ))
-
     uav_name = LaunchConfiguration("uav_name")
 
     # -------------------------------------------------
     # run type
     # -------------------------------------------------
-
     ld.add_action(DeclareLaunchArgument(
         "run_type",
         default_value="simulation"
     ))
-
     run_type = LaunchConfiguration("run_type")
 
     # -------------------------------------------------
-    # standalone container
+    # config file (DEFAULT + OVERRIDE SUPPORT)
     # -------------------------------------------------
+    ld.add_action(DeclareLaunchArgument(
+        "config",
+        default_value=os.path.join(pkg_path, "config", "default.yaml")
+    ))
+    config = LaunchConfiguration("config")
 
+    # -------------------------------------------------
+    # standalone mode
+    # -------------------------------------------------
     ld.add_action(DeclareLaunchArgument(
         "standalone",
         default_value="true"
     ))
-
     standalone = LaunchConfiguration("standalone")
 
     # -------------------------------------------------
-    # config
+    # container name (if attaching externally)
     # -------------------------------------------------
-
     ld.add_action(DeclareLaunchArgument(
-        "custom_config",
+        "container_name",
         default_value=""
     ))
-
-    custom_config = LaunchConfiguration("custom_config")
+    container_name = LaunchConfiguration("container_name")
 
     # -------------------------------------------------
     # RBL Controller component
     # -------------------------------------------------
-
     rbl_controller_node = ComposableNode(
 
         package=pkg_name,
@@ -75,19 +74,17 @@ def generate_launch_description():
         namespace=uav_name,
 
         parameters=[
-            {"config": os.path.join(pkg_path, "config", "default.yaml")},
-            {"custom_config": custom_config},
+            {"config": config},
             {"simulation": True},
             {"uav_name": uav_name},
             {"control_frame": [uav_name, "/world_origin"]},
         ],
 
         remappings=[
-            ("~/odom_in","estimation_manager/odom_main"),
-            ("~/alt_in","estimation_manager/garmin_agl/agl_height"), 
-            ("~/pcl_in","losos_server/current_submap_pc"), 
-            # ("~/pcl_in","livox/points"), 
-            ("~/octomap_in","octomap_server/octomap_local_binary"), 
+            ("~/odom_in", "estimation_manager/odom_main"),
+            ("~/alt_in", "estimation_manager/garmin_agl/agl_height"),
+            ("~/pcl_in", "losos_server/current_submap_pc"),
+            ("~/octomap_in", "octomap_server/octomap_local_binary"),
             ("~/group_states_in", "filter_reflective_uavs/pose_vel"),
             ("~/tracker_cmd_in", "control_manager/tracker_cmd"),
             ("~/ref_out", "control_manager/reference"),
@@ -107,22 +104,9 @@ def generate_launch_description():
     )
 
     # -------------------------------------------------
-    # container name
+    # load into external container (if used)
     # -------------------------------------------------
-
-    ld.add_action(DeclareLaunchArgument(
-        "container_name",
-        default_value=""
-    ))
-
-    container_name = LaunchConfiguration("container_name")
-
-    # -------------------------------------------------
-    # load into existing container
-    # -------------------------------------------------
-
     ld.add_action(
-
         LoadComposableNodes(
             target_container=container_name,
             composable_node_descriptions=[rbl_controller_node],
@@ -131,17 +115,14 @@ def generate_launch_description():
     )
 
     # -------------------------------------------------
-    # standalone container
+    # standalone container (default mode)
     # -------------------------------------------------
-
     ld.add_action(
-
         ComposableNodeContainer(
             namespace=uav_name,
             name="rbl_controller_container",
             package="rclcpp_components",
             executable="component_container_mt",
-            # prefix=['gdb -q -ex "set pagination off" -ex "handle SIGABRT stop print nopass" -ex run -ex bt -ex "thread apply all bt" --args'],
             composable_node_descriptions=[rbl_controller_node],
             output="screen",
             condition=IfCondition(standalone)
@@ -149,81 +130,3 @@ def generate_launch_description():
     )
 
     return ld
-
-
-# old launch file:
-
-# <launch>
-
-#     <!-- defines name of the namespace of the drone -->
-#     <arg name="UAV_NAME" default="$(optenv UAV_NAME)" />
-#     <arg name="RUN_TYPE" default="simulation" />
-
-#     <arg name="custom_config" default="" />
-
-#     <!-- will it run using GNU debugger? -->
-#     <arg name="DEBUG" default="false" />
-#     <arg unless="$(arg DEBUG)" name="launch_prefix_debug" value="" />
-#     <arg if="$(arg DEBUG)" name="launch_prefix_debug" value="debug_roslaunch" />
-
-#     <!-- will it run as standalone nodelet or using a nodelet manager? -->
-#     <arg name="standalone" default="true" />
-#     <arg name="manager" default="$(arg UAV_NAME)_waypointflier_manager" />
-#     <arg name="n_threads" default="8" />
-#     <arg unless="$(arg standalone)" name="nodelet" value="load" />
-#     <arg if="$(arg standalone)" name="nodelet" value="standalone" />
-#     <arg unless="$(arg standalone)" name="nodelet_manager" value="$(arg manager)" />
-#     <arg if="$(arg standalone)" name="nodelet_manager" value="" />
-
-#     <!-- Namespace - all topics, services and parameters will be remapped using this namespace as a prefix (eg.: "/waypoint_flier/odom_uav_in" to "/uav1/waypoint_flier/odom_uav_in") -->
-#     <group ns="$(arg UAV_NAME)">
-
-#         <!-- FormationPlanning nodelet -->
-#         <node pkg="nodelet" type="nodelet" name="rbl_controller"
-#             args="$(arg nodelet) formation_control/RBLController $(arg nodelet_manager)"
-#             launch-prefix="$(arg launch_prefix_debug)" output="screen">
-
-#         <!-- lidar clustering to measure distance to leader -->
-#         <!-- <node name="lidar_clusters$(arg UAV_NAME)" pkg="rbl_controller" type="lidar_clusters" output="screen"/> -->
-#             <!-- Parameters loaded from launch file -->
-#             <param name="simulation" value="$(eval arg('RUN_TYPE') == 'simulation')" />
-#             <param name="uav_name" value="$(arg UAV_NAME)" />
-#             <!-- TODO: move this to config .yaml so you can change if simulation or experiment -->
-#             <param name="control_frame" value="$(arg UAV_NAME)/world_origin" />
-#             <!-- <param name="control_frame" value="$(arg UAV_NAME)/hector_origin" /> -->
-
-#             <!-- ROS parameters config file -->
-#             <rosparam file="$(find rbl_controller)/config/default.yaml" />
-#             <rosparam file="$(arg custom_config)" />
-#             <!-- <rosparam file="$(find formation_controller_susd)/config/$(arg RUN_TYPE).yaml" /> -->
-
-#             <!-- Subscribers (note that the topic names will also be remapped using the namespace as a prefix) -->
-#             <remap from="~tracker_cmd_in" to="control_manager/tracker_cmd" />
-
-#             <!-- Service clients (note that the service names will also be remapped using the namespace as a prefix) -->
-#             <remap from="~ref_pos_out" to="control_manager/reference" />
-#             <remap from="~goto_out" to="control_manager/goto" />
-#             <!-- Service servers (note that the service names will also be remapped using the namespace as a prefix) -->
-#             <remap from="~control_activation_in" to="~activation" />
-#             <remap from="~control_activation_params_in" to="~activation_params" />
-#             <remap from="~control_deactivation_in" to="~deactivation" />
-#             <remap from="~fly_to_start_in" to="~fly_to_start" />
-
-#             <!-- Publishers (note that the service names will also be remapped using the namespace as a prefix) -->
-#             <remap from="~destination_out" to="~destination_vis" />
-#             <remap from="~centroid_out" to="~centroid_vis" />
-#             <remap from="~position_out" to="~position_vis" />
-#             <remap from="~obstacle_markers_out" to="~obstacles_vis" />
-#             <remap from="~neighbors_markers_out" to="~neighbors_vis" />
-#             <remap from="~hull_markers_out" to="~hull_vis" />
-#             <remap from="~line_out" to="~line_vis" />
-
-            
-
-            
-
-#         </node>
-
-#     </group>
-
-# </launch>
